dbc731d48614e132084114e76bc538d5383b7759
Guoshun Nan <nanguoshun@gmail.com>
2018-08-02 23:29:55 +0800

    update

---
+++ src/example/linear_crf_feature_manager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by  ngs on 02/08/2018.
+//
+
+#include "linear_crf_feature_manager.h"
+
+LinearCRFFeatureManager::LinearCRFFeatureManager(GlobalNetworkParam *ptr_param):FeatureManager(ptr_param){
+
+}
+
+LinearCRFFeatureManager::~LinearCRFFeatureManager() {
+
+}
